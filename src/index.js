5fcfc9d022eb80fb837e57efc6e279203c9d3ac8
luoxiu_yuan <luoxiu_yuan@kingdee.com>
2019-10-16 20:09:54 +0800

    【ylxreact】input表单内容

--- src/index.js
+++ src/index.js
@@ -4,10 +4,11 @@ import './index.css';
 import './cssTest/outset.css'
 // import App from './App';
 // import PostList from './jsTest/PostList2'
-import MyComponent from  './event/jiantou'
+// import MyComponent from  './event/jiantou'
+import LoginForm from './form/input.js'
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(< MyComponent />, document.getElementById('root'));
+ReactDOM.render(< LoginForm />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
